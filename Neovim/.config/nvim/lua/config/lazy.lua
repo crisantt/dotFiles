4bb421084a0212688a46b201be8b56863dfff964
crisantt <jcsanjose@proton.me>
2025-10-19 08:19:07 +0000

    local lsp added

--- Neovim/.config/nvim/lua/config/lazy.lua
+++ Neovim/.config/nvim/lua/config/lazy.lua
@@ -14,6 +14,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+require("config.lsp")
 require("config.globals")
 require("config.keymaps")
 require("config.options")
---
+++ Neovim/.config/nvim/lua/config/lsp.lua
@@ -0,0 +1,26 @@
+vim.lsp.enable('lua_ls')
+vim.lsp.enable('gopls')
+
+vim.diagnostic.config({
+    --     virtual_texts = true,
+    virtual_lines = true,
+    underline = true,
+    update_in_insert = false,
+    severity_sort = true,
+    float = {
+        border = "rounded",
+        source = true,
+    },
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = "󰅚 ",
+            [vim.diagnostic.severity.WARN] = "󰀪 ",
+            [vim.diagnostic.severity.INFO] = "󰋽 ",
+            [vim.diagnostic.severity.HINT] = "󰌶 ",
+        },
+        numhl = {
+            [vim.diagnostic.severity.ERROR] = "ErrorMsg",
+            [vim.diagnostic.severity.WARN] = "WarningMsg",
+        },
+    },
+})
