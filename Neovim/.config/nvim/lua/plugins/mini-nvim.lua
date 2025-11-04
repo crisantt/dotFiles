d1d7175d1fb7689c75c50bc5c067d7bfc219de78
crisantt <jcsanjose@proton.me>
2025-11-05 01:35:48 +0800

    mini nvim & alacritty update

---
+++ Neovim/.config/nvim/lua/plugins/mini-nvim.lua
@@ -0,0 +1,5 @@
+return{
+   { "echasnovski/mini.indentscope", version = "*", opts = {} },
+   { 'nvim-mini/mini.pairs', version = '*', opts = {} },
+   { "echasnovski/mini.trailspace", version = "*", opts = {} },
+}
